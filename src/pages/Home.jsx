234c4182f3bee9d54e3f1b38a772ac9d85dcceac
Om-Vilas-Kulkarni <kulkarniom-cmpn@atharvacoe.ac.in>
2024-02-25 00:53:46 +0530

    Why to attend tedx section added
    Why to attend tedx section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/home/HeroSection";
 import SpeakerContent from "../components/speakers/SpeakerContent";
 import Sponsors from "../components/home/Sponsors";
 import { useSpeakerData, useSponsorData } from "../Context/ContextProvider";
+import Reasons from "../components/home/Reasons";
 
 const Home = () => {
   const { speakers } = useSpeakerData();
@@ -11,6 +12,7 @@ const Home = () => {
     <main className="overflow-x-hidden bg-tedx-bg">
       <HeroSection />
       <SpeakerContent speakers={speakers} />
+      <Reasons />
       <Sponsors sponsors={sponsors} />
     </main>
   );
