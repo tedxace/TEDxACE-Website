33a60732a7ae6b2fee40863fd00ed3185acbac5e
Shivam Sanjay Narkar <shivam.narkar@vpt.edu.in>
2024-02-29 10:30:40 +0530

    Soem changes

--- src/components/home/ParallaxSection.jsx
+++ src/components/home/ParallaxSection.jsx
@@ -10,7 +10,7 @@ const images = [
   "bg.jpeg",
   "bg.jpeg",
   "bg.jpeg",
-  "bg.jpeg",
+  "footer.png",
   "bg.jpeg",
   "bg.jpeg",
   "bg.jpeg",
@@ -37,7 +37,7 @@ export default function ParallaxSection() {
   const y = useTransform(scrollYProgress, [0, 1], [height, height * -0.6]);
   const y2 = useTransform(scrollYProgress, [0, 1], [0, height]);
   const y3 = useTransform(scrollYProgress, [0, 1], [height, height * -0.6]);
-  // const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 1]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 1]);
 
   useEffect(() => {
     const resize = () => {
@@ -55,22 +55,22 @@ export default function ParallaxSection() {
   return (
     <main className="font-sans">
       <div
-        className="h-105vh relative overflow-hidden bg-cover bg-fixed bg-gradient-to-t from-tedx-bg/100 to-transparent"
+        className="h-105vh relative overflow-hidden bg-cover bg-fixed bg-gradient-to-t from-tedx-bg/30 to-transparent"
         ref={ref}
       >
-        <div className="absolute -top-[12.5vh] h-200vh flex justify-center items-center gap-4vw p-2vw">
+        <div className="absolute -top-[12.5vh] w-full px-20 h-200vh flex justify-center items-center gap-4vw p-2vw">
           <Column images={[images[0], images[1], images[2]]} y={y} />
           {window.innerWidth > 450 && (
             <Column images={[images[3], images[4], images[5]]} y={y2} />
           )}
-
+          {/* 
           {window.innerWidth > 768 && (
             <Column images={[images[6], images[7], images[8]]} y={y3} />
           )}
-          {/* <Column images={[images[9], images[10], images[11]]} y={y4} /> */}
+          <Column images={[images[9], images[10], images[11]]} y={y4} /> */}
         </div>
-        <div className=" h-screen sm:h-[120vh] flex  justify-center items-center  ">
-          <div className="p-4 sticky text-5xl bg-black w-1/2 h-1/4 flex items-center justify-center text-center font-bold ">
+        <div className=" h-screen sm:h-[120vh] bg-gradient-to-t from-black/40 to-black/70 flex  justify-center items-center  ">
+          <div className="p-4 sticky text-5xl bg-gradient-to-tr w-full h-full from-black/40 to-black/70   flex items-center justify-center text-center font-bold ">
             Hello
           </div>
           {/* <div className="absolute h-full w-full bg-gradient-to-t from-black/30 via-transparent to-black/60"></div> */}
@@ -90,7 +90,7 @@ const Column = ({ images, y, className }) => {
   return (
     <motion.div
       ref={ref}
-      className="sm:w-1/2 duration-300 w-full ease-in min-w-450px   flex-col gap-2vw whitespace-nowrap"
+      className="sm:w-1/2 duration-300 w-full ease-linear min-w-450px   flex-col gap-2vw whitespace-nowrap"
       style={{ y }}
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 100 }}
